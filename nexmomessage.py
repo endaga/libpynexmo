91bfa0124ab98b38aa15e8ed36f66e27ca8b0492
Kurtis Heimerl <kheimerl@cs.berkeley.edu>
2013-01-01 19:37:47 -0800

    adding more functionality

--- nexmomessage.py
+++ nexmomessage.py
@@ -5,6 +5,8 @@ import json
 
 BASEURL = "https://rest.nexmo.com"
 
+class NexmoException(Exception):
+    pass
 
 class NexmoMessage:
 
@@ -25,7 +27,9 @@ class NexmoMessage:
         self.apireqs = [
             'balance',
             'pricing',
-            'numbers'
+            'numbers',
+            'search',
+            'buy'
         ]
         self.reqtypes = [
             'json',
@@ -73,38 +77,40 @@ class NexmoMessage:
             http://www.nexmo.com/documentation/api/ """
         # mandatory parameters for all requests
         if not self.sms.get('username') or not self.sms.get('password'):
-            return False
+            raise NexmoException("No username or password")
 
         # API requests handling
         if self.sms['type'] in self.apireqs:
             if self.sms['type'] == 'balance' or self.sms['type'] == 'numbers':
                 return True
-            elif self.sms['type'] == 'pricing' and not self.sms.get('country'):
-                return False
+            elif self.sms['type'] in ['pricing', 'search'] and not self.sms.get('country'):
+                raise NexmoException("Pricing/search needs country")
+            elif self.sms['type'] in ['buy'] and (not self.sms.get('country') or not self.sms.get('msisdn')):
+                raise NexmoException("Buy needs country and msisdn")
             return True
         # SMS logic, check Nexmo doc for details
         elif self.sms['type'] not in self.smstypes:
-            return False
+            raise NexmoException("Invalid Type")
         elif self.sms['type'] == 'text' and not self.sms.get('text'):
-            return False
+            raise NexmoException("Missing Text")
         elif self.sms['type'] == 'binary' and (not self.sms.get('body') or \
                 not self.sms.get('udh')):
-            return False
+            raise NexmoException("Missing Binary")
         elif self.sms['type'] == 'wappush' and (not self.sms.get('title') or \
                 not self.sms.get('url')):
-            return False
+            raise NexmoException("Missing title/url")
         elif self.sms['type'] == 'vcal' and not self.sms.get('vcal'):
-            return False
+            raise NexmoException("Missing vcal")
         elif self.sms['type'] == 'vcard' and not self.sms.get('vcard'):
-            return False
+            raise NexmoException("Missing vcard")
         elif not self.sms.get('from') or not self.sms.get('to'):
-            return False
+            raise NexmoException("Missing from or to")
         return True
 
     def build_request(self):
         # check SMS logic
         if not self.check_sms():
-            return False
+            raise NexmoException("Invalid SMS")
         elif self.sms['type'] in self.apireqs:
             # basic API requests
             # balance
@@ -120,11 +126,21 @@ class NexmoMessage:
             elif self.sms['type'] == 'numbers':
                 self.request = "%s/account/numbers/%s/%s" % (BASEURL,
                     self.sms['username'], self.sms['password'])
+            # search
+            elif self.sms['type'] == 'search':
+                self.request = "%s/number/search/%s/%s/%s" \
+                    % (BASEURL, self.sms['username'], self.sms['password'],
+                       self.sms['country'])
+            # search
+            elif self.sms['type'] == 'buy':
+                self.request = "%s/number/buy/%s/%s/%s/%s" \
+                    % (BASEURL, self.sms['username'], self.sms['password'],
+                       self.sms['country'], self.sms['msisdn'])
             return self.request
         else:
             # standard requests
             if self.sms['reqtype'] not in self.reqtypes:
-                return False
+                raise NexmoException("Invalid reqtype")
             params = self.sms.copy()
             params.pop('reqtype')
             params.pop('server')
@@ -138,7 +154,7 @@ class NexmoMessage:
 
     def send_request(self):
         if not self.build_request():
-            return False
+            raise NexmoException("Failed to build")
         if self.sms['reqtype'] == 'json':
             return self.send_request_json(self.request)
         elif self.sms['reqtype'] == 'xml':
@@ -148,10 +164,7 @@ class NexmoMessage:
         url = request
         req = urllib2.Request(url=url)
         req.add_header('Accept', 'application/json')
-        try:
-            return json.load(urllib2.urlopen(req))
-        except ValueError:
-            return False
+        return json.load(urllib2.urlopen(req))
 
     def send_request_xml(self, request):
         return "XML request not implemented yet."
--- nexmosample.py
+++ nexmosample.py
@@ -1,73 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import sys, random, string
 
 from nexmomessage import NexmoMessage
 
-
-def main():
+def main(function, args):
 
     r = "json"
-    u = "changeme"
-    p = "changeme"
-    f = "marcuz"
-    t = "+39**********"
-    m = "fool the reader àèìòù !!!"
-    bb = "0011223344556677"
-    bu = "06050415811581"
+    u = "XXXXXXXX"
+    p = "XXXXXXXX"
+    f = "4444444444"
+    m = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(20))
 
     msg = {'reqtype': r, 'password': p, 'from': f, 'to': t, 'username': u}
+    req = {'password': p, 'username': u}
 
-    # account balance
-    req = {'password': p, 'username': u, 'type': 'balance'}
-    print("request details: %s") % NexmoMessage(req).get_details()
-    print NexmoMessage(req).send_request()
-
-    print
-
-    # my numbers
-    req = {'password': p, 'username': u, 'type': 'numbers'}
-    print("request details: %s") % NexmoMessage(req).get_details()
-    print NexmoMessage(req).send_request()
-
-    print
-
-    # pricing for country 'NL'
-    req['type'] = 'pricing'
-    req['country'] = 'NL'
-    print("request details: %s") % NexmoMessage(req).get_details()
-    print NexmoMessage(req).send_request()
-
-    print
-
-    sys.exit()
-
-    # text message
-    msg['text'] = m
-    sms1 = NexmoMessage(msg)
-    print("SMS details: %s") % sms1.get_details()
-    m += " ktnxbye"
-    sms1.set_text_info(m)
-    print("SMS details: %s") % sms1.get_details()
-    print sms1.send_request()
-
-    print
-
-    # bin message
-    sms2 = NexmoMessage(msg)
-    sms2.set_bin_info(bb, bu)
-    print("SMS details: %s") % sms2.get_details()
-    print sms2.send_request()
-
-    print
-
-    # wap message
-    msg['title'] = "this is a test"
-    msg['url'] = "http://twitter.com/tmarcuz"
-    msg['text'] = False
-    sms3 = NexmoMessage(msg)
-    print("SMS details: %s") % sms3.get_details()
-    print sms3.send_request()
+    if (function == "balance"):
+        # account balance
+        req['type'] = 'balance'
+        print("request details: %s") % NexmoMessage(req).get_details()
+        print NexmoMessage(req).send_request()
+    elif (function == "numbers"):
+        # my numbers
+        req['type'] = 'numbers'
+        print("request details: %s") % NexmoMessage(req).get_details()
+        print NexmoMessage(req).send_request()
+    elif (function == "pricing"):
+        # pricing for country 'NL'
+        req['type'] = 'pricing'
+        req['country'] = args[0]
+        print("request details: %s") % NexmoMessage(req).get_details()
+        print NexmoMessage(req).send_request()
+    elif (function == "search"):
+        # searching for numbers for country 'NL'
+        req['type'] = 'search'
+        req['country'] = args[0]
+        print("request details: %s") % NexmoMessage(req).get_details()
+        print NexmoMessage(req).send_request()
+    elif (function == "buy"):
+        # buying number for country
+        req['type'] = 'buy'
+        req['country'] = args[0]
+        req['msisdn'] = args[1]
+        print("request details: %s") % NexmoMessage(req).get_details()
+        print NexmoMessage(req).send_request()
+    elif (function == "message"):
+        # text message
+        msg['text'] = m
+        sms1 = NexmoMessage(msg)
+        print("SMS details: %s") % sms1.get_details()
+        m += " ktnxbye"
+        sms1.set_text_info(m)
+        print("SMS details: %s") % sms1.get_details()
+        print sms1.send_request()
+    elif (function == "binary"):
+        # bin message
+        sms2 = NexmoMessage(msg)
+        sms2.set_bin_info(bb, bu)
+        print("SMS details: %s") % sms2.get_details()
+        print sms2.send_request()
+    elif (function == "wap"):
+        # wap message
+        msg['title'] = "this is a test"
+        msg['url'] = "http://twitter.com/tmarcuz"
+        msg['text'] = False
+        sms3 = NexmoMessage(msg)
+        print("SMS details: %s") % sms3.get_details()
+        print sms3.send_request()
 
 if __name__ == "__main__":
-    sys.exit(main())
+    if (len(sys.argv) < 2):
+        print "provide function"
+        exit(1)
+    sys.exit(main(sys.argv[1], sys.argv[2:]))
